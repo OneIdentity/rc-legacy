7f7a2df871d69c4eb07770592c26ecf5d50de8cc
tedjp <tedp>
2006-03-28 07:01:57 +0000

    Delete _more_ auto-generated files (in include/) This is getting a bit repetitive.

--- heimdal/include/version.h
+++
@@ -1,2 +0,0 @@
-const char *heimdal_long_version = "@(#)$Version: vas-apps 0.7.2q1 by tedp on kookaburra (i686-pc-linux-gnu) Tue Mar  7 14:38:59 EST 2006 $";
-const char *heimdal_version = "vas-apps 0.7.2q1";
