cd79b34326786c9d83e8f2836cf9c0753f96b965
gelgey <gelgey>
2006-10-26 22:57:54 +0000

    Disable username widget until its associated prompt is displayed. Prevents username entry before PAM conversation occurs (ie, which may not be necessary if inspecting a smartcard)

--- gdm-2.6.0.5/gui/greeter/greeter_item_pam.c
+++ gdm-2.6.0.5/gui/greeter/greeter_item_pam.c
@@ -209,11 +209,21 @@ greeter_item_pam_prompt (const char *message,
     {
       entry = GNOME_CANVAS_WIDGET (entry_info->item)->widget;
       
-      gtk_entry_set_visibility (GTK_ENTRY (entry), entry_visible);
-      gtk_widget_set_sensitive (GTK_WIDGET (entry), TRUE);
-      gtk_entry_set_max_length (GTK_ENTRY (entry), entry_len);
-      gtk_entry_set_text (GTK_ENTRY (entry), "");
-      gtk_widget_grab_focus (entry);
+      if (message != NULL && strcmp (message, "") != 0) 
+        {
+          gtk_entry_set_visibility (GTK_ENTRY (entry), entry_visible);
+          gtk_widget_set_sensitive (GTK_WIDGET (entry), TRUE);
+          gtk_entry_set_max_length (GTK_ENTRY (entry), entry_len);
+          gtk_entry_set_text (GTK_ENTRY (entry), "");
+          gtk_widget_grab_focus (entry);
+        }
+      else
+        {
+          gtk_entry_set_visibility (GTK_ENTRY (entry), entry_visible);
+          gtk_entry_set_text (GTK_ENTRY (entry), "...");
+          gtk_widget_set_sensitive (GTK_WIDGET (entry), FALSE);
+        }
+
     }
 
   messages_to_give = FALSE;
