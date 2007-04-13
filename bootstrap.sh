bf6a1eac30290eb76ea8376ff9da2d9805693fdd
dleonard0 <David.Leonard@quest.com>
2007-04-13 05:32:53 +0000

    fetch polypkg during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 set -ex
+
+#-- Builds the automadness
 rm -rf autom4te.cache install-sh missing Makefile.in configure \
        aclocal.m4 config.h.in
 autoreconf --install
+
+#-- Fetches polypkg
+test -x pp ||
+    { wget http://rc.vintela.com/pub/rc/polypkg/pp && 
+      chmod +x pp &&
+      ./pp --version; }
