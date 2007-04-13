131a461872b6541db810f5ea48806fef789c04ab
dleonard0 <David.Leonard@quest.com>
2007-04-13 15:20:31 +0000

    remove a problem about constants not being found; improve warning level

--- tests/driver.php
+++ tests/driver.php
@@ -1,7 +1,9 @@
 <?
     /* This script is run by driver before any test script */
 
-    /* if (!extension_loaded("vas"))
-	dl("vas.so"); */
+    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    if (!extension_loaded("vas"))
+	dl("vas.so");
+
 ?>
 
