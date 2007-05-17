2bff0fec5a61ed62853724743c2472c315377723
dleonard0 <David.Leonard@quest.com>
2007-05-17 02:12:33 +0000

    Use symlinks to pp to improve build time

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,13 @@ rm -rf autom4te.cache install-sh missing Makefile.in configure \
 autoreconf --install
 
 #-- Fetches polypkg
-test -x pp ||
-    { wget http://rc.vintela.com/pub/rc/polypkg/pp && 
-      chmod +x pp &&
-      ./pp --version; }
+if test ! -x pp; then
+    if test -x /data/rc/pub/rc/polypkg/pp; then
+	#-- /data/rc/pub is an NFS share within Quest
+	ln -s /data/rc/pub/rc/polypkg/pp pp
+    else
+	wget http://rc.quest.com/pub/rc/polypkg/pp
+	chmod +x pp
+    fi
+    ./pp --version
+fi
