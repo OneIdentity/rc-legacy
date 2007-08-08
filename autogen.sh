2521cf7cb4aad5123d2847565ce731f2915de04d
dleonard0 <David.Leonard@quest.com>
2007-08-08 08:07:19 +0000

    Add an autogen script that builds configure, Makefiles, config.h

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf config.h stamp-h1 config.status config.cache config.log \
+       configure.lineno configure.status.lineno autom4te.cache .deps \
+       Makefile
+
+autoreconf --install
